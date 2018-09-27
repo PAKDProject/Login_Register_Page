f1f1236d53039382261d0dd468bec9c95bc2e4b3
S00170565 <S00170565@mail.itsligo.ie>
2018-09-27 19:17:34 +0100

    added some validation

--- js/main.js
+++ js/main.js
@@ -5,4 +5,22 @@ $(document).ready(() => {
         $("#loginForm").toggleClass("hidden");
         $("#registerForm").toggleClass("hidden");
     })
+
+    $(".btnSubmit").click(function(event) {
+
+        // Fetch form to apply custom Bootstrap validation
+        var form = $("#myForm")
+        var registerForm = $("#registerForm")
+    
+        if (form[0].checkValidity() === false || registerForm[0].checkValidity() === false) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        
+        form.addClass('was-validated');
+        registerForm.addClass('was-validated');
+    });
+    
 });
+
+
