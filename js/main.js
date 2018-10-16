cee8989c802109ad3065ebd569acc61b71adbddd
S00170565 <S00170565@mail.itsligo.ie>
2018-10-16 15:50:46 +0100

    added confirmation page

--- js/main.js
+++ js/main.js
@@ -6,49 +6,9 @@ $(document).ready(() => {
         $("#registerForm").toggleClass("hidden");
     })
 
-    $("#btnSubmit").click(function(event) {
+    $(".btnSubmit").click(() => {
+        $("#registerForm").toggleClass("hidden");
+        $("#confirmationForm").toggleClass("hidden");
+    })
 
-        // Fetch form to apply custom Bootstrap validation
-        var form = $("#myForm")
-        //Add form validation
-        if (form[0].checkValidity() === false ) {
-            event.preventDefault()
-            event.stopPropagation()
-            break;
-          }
-        form.addClass('was-validated');
-
-        //Read all fields and create object.
-        const email = $('#email').val();
-        const pass = $('#password').val();
-        const apiRoute = "http://localhost:8080/login"
-        const loginDetails = {
-            email : email,
-            pass : password
-        };
-
-        //Fetch request to server.
-        fetch(apiRoute, {
-            method: "POST",
-            mode: 'cors',
-            body: JSON.stringify(loginDetails),
-            headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            }
-        }).then((res) => {
-            res = res.json();
-            const token = res.body;
-
-            if(res.status === 200) {
-                //save the token etc
-            }
-            else if (res.status === 404) {
-                console.log("User not found!")
-            }
-        })
-
-    });
-    
 });
-
-
