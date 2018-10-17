1f827caacc9084945a640c18cb4621e8c546001e
S00170565 <S00170565@mail.itsligo.ie>
2018-10-17 13:54:32 +0100

    started refactoring

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+//Login Form
+document.getElementById("formLogin").addEventListener("submit", loginFormSubmit, false);
+//Register Form
+document.getElementById("formRegister").addEventListener("submit", registerFormSubmit, false);
+//Confirmation Code part
+document.getElementById("formConfirm").addEventListener("submit", confirmationFormSubmit, false);
+
 $(document).ready(() => {
   //Show or hide form based on selections on page
   $("#toggleRegisterPage").click(() => {
@@ -5,20 +12,14 @@ $(document).ready(() => {
     $("#registerForm").toggleClass("hidden");
   });
 
-  $("#registerFormSubmit").click(() => {
-    $("#registerForm").toggleClass("hidden");
-    $("#confirmationForm").toggleClass("hidden");
-  });
-
-  $("#confirmFormSubmit").click(() => {
+  $("#alreadyHaveAccount").click(() => {
     $("#loginForm").toggleClass("hidden");
-    $("#confirmationForm").toggleClass("hidden");
-  });
+    $("#registerForm").toggleClass("hidden");
+  })
 });
 
-function validateForm(form) {}
-//Login Form
-document.getElementById("formLogin").addEventListener("submit", e => {
+
+function loginFormSubmit(e) {
   const form = document.getElementById("formLogin");
   const formElements = document.getElementById("formLogin").elements;
   const postData = {};
@@ -26,37 +27,40 @@ document.getElementById("formLogin").addEventListener("submit", e => {
   if (form.checkValidity() === false) {
     event.preventDefault();
     event.stopPropagation();
+    form.classList.add("was-validated");
+
+  } else {
+
+    form.classList.add("was-validated");
+    for (var i = 0; i < formElements.length; i++) {
+      if (formElements[i].type != "submit")
+        postData[formElements[i].name] = formElements[i].value;
+    }
+    const loginObject = {
+      email: postData.email,
+      password: postData.password
+    };
+
+    console.log(loginObject);
+
+    const apiUrl = "";
+
+    fetch(apiUrl, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify(loginObject)
+    }).then(res => {
+      console.log(res);
+    });
+
+    e.preventDefault();
   }
-  form.classList.add("was-validated");
-  for (var i = 0; i < formElements.length; i++) {
-    if (formElements[i].type != "submit")
-      postData[formElements[i].name] = formElements[i].value;
-  }
-  const loginObject = {
-    email: postData.email,
-    password: postData.password
-  };
+};
 
-  console.log(loginObject);
-
-  const apiUrl = "";
-
-  fetch(apiUrl, {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: JSON.stringify(loginObject)
-  }).then(res => {
-    console.log(res);
-  });
-
-  e.preventDefault();
-});
-
-//Register Form
-document.getElementById("formRegister").addEventListener("submit", e => {
+function registerFormSubmit(e) {
   const form = document.getElementById("formRegister");
   const formElements = document.getElementById("formRegister").elements;
   const postData = {};
@@ -64,61 +68,78 @@ document.getElementById("formRegister").addEventListener("submit", e => {
   if (form.checkValidity() === false) {
     event.preventDefault();
     event.stopPropagation();
+    form.classList.add("was-validated")
+  } else {
+
+    form.classList.add("was-validated");
+    for (var i = 0; i < formElements.length; i++) {
+      if (formElements[i].type != "submit")
+        postData[formElements[i].name] = formElements[i].value;
+    }
+
+    const registrationObject = {
+      email: postData.regEmail,
+      name: postData.regFName,
+      family_name: postData.regSName,
+      password: postData.regPassword
+    };
+    console.log(registrationObject);
+
+    const apiUrl = ""; //Enter endpoint
+
+    fetch(apiUrl, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify(registrationObject)
+    }).then(res => {
+      console.log(res);
+    });
+
+    e.preventDefault();
+    $("#registerForm").toggleClass("hidden");
+    $("#confirmationForm").toggleClass("hidden");
   }
-  form.classList.add("was-validated");
-  for (var i = 0; i < formElements.length; i++) {
-    if (formElements[i].type != "submit")
-      postData[formElements[i].name] = formElements[i].value;
-  }
+};
 
-  const registrationObject = {
-    email: postData.regEmail,
-    name: postData.regFName,
-    family_name: postData.regSName,
-    password: postData.regPassword
-  };
-  console.log(registrationObject);
-
-  const apiUrl = ""; //Enter endpoint
-
-  fetch(apiUrl, {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: JSON.stringify(registrationObject)
-  }).then(res => {
-    console.log(res);
-  });
-
-  e.preventDefault();
-});
-
-//Confirmation Code part
-document.getElementById("formConfirm").addEventListener("submit", e => {
+function confirmationFormSubmit(e) {
+  const form = document.getElementById("formConfirm");
   var formElements = document.getElementById("formConfirm").elements;
   var postData = {};
-  for (let i = 0; i < formElements.length; i++) {
-    if (formElements[i].type !== "submit")
-      postData[formElements[i].name] = formElements[i].value;
+
+
+  if (form.checkValidity() === false) {
+    isValid = false;
+    event.preventDefault();
+    event.stopPropagation();
+    form.classList.add("was-validated")
+  } else {
+    $("#loginForm").toggleClass("hidden");
+    $("#confirmationForm").toggleClass("hidden");
+    for (let i = 0; i < formElements.length; i++) {
+      if (formElements[i].type !== "submit")
+        postData[formElements[i].name] = formElements[i].value;
+    }
+
+    const confirmationCode = postData.confirmationCode;
+    console.log(confirmationCode);
+
+    const apiUrl = ""; //endpoint
+
+    fetch(apiUrl, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify(confirmationCode)
+    }).then(res => {
+      console.log(res);
+    });
+
+    e.preventDefault();
   }
 
-  const confirmationCode = postData.confirmationCode;
-  console.log(confirmationCode);
-
-  const apiUrl = ""; //endpoint
-
-  fetch(apiUrl, {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: JSON.stringify(confirmationCode)
-  }).then(res => {
-    console.log(res);
-  });
-
-  e.preventDefault();
-});
+};
